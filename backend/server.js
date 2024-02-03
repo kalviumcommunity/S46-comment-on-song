8b49dc7b372a2ac0587b257bdc52c633293b211e
Alwin Sunil <alv12alwin@gmail.com>
2024-02-03 18:42:06 +0530

    Implemented mongoDB client

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const { startDB, stopDB, isConnected } = require("./db");
+
 const app = express();
 const port = 3000;
 
+startDB();
+
+app.get("/", (req, res) => {
+	res.json({
+		"📦 Database connection status": isConnected()
+			? "✅ Connected"
+			: "❌ Not connected",
+	});
+});
+
 app.get("/ping", (req, res) => {
 	res.send("pong");
+});
+
+app.get("/start", async (req, res) => {
+	let status = await startDB();
+	res.send(status);
+});
+
+app.get("/close", async (req, res) => {
+	let status = await stopDB();
+	res.send(status);
 });
 
 app.listen(port, () => {
