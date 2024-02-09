0af756f3339eaaeefece90b2cfe86aeb2581f63b
Alwin Sunil <alv12alwin@gmail.com>
2024-02-09 16:39:06 +0530

    Fixed typo in filepath

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom"
-import Home from "@/Pages/Home/Home"
+import Home from "@/pages/Home"
 import Header from "@/components/Header"
 import "./App.css"
 
