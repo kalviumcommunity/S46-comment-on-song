0a016cd3f640b3be857811335fb2a577cc1ca83a
Alwin Sunil <alv12alwin@gmail.com>
2024-02-14 15:55:07 +0530

    Updated env variable in db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 const startDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("📦 Connected to mongoDB")
         return "📦 Connected to mongoDB"
     } catch (err) {
